8b2a76e110da8120760ad5d26a44a4a2a622dd66
Tom <tom.stanley37@gmail.com>
2018-06-19 19:12:16 +0100

    get the "log in" switch to the chat page working

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-const express = require("express");
+const app = require("express")();
+const http = require('http').Server(app);
 const bodyParser = require("body-parser");
-const app = express();
+const cookieParser = require("cookie-parser");
+
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 // Serve a page
 app.get('/', (req, res) => {
-  res.render("app");
+  res.render("login");
+});
+
+app.get('/chat', (req, res) => {
+  if (req.cookies.character) {
+    const locals = { "character": req.cookies.character };
+    return res.render("app", locals);
+  } else {
+    return res.redirect("/");
+  }
 });
 
 // React to post requests coming in
 app.post('/', (req, res) => {
-  console.dir(req.body);
-  res.render("app");
+  console.dir(`Coming from /index: ${req.body}`);
+  res.cookie("character", req.body.character);
+  return res.redirect("/chat");
 });
+
+app.post('/chat', (req, res) => {
+  console.dir(`Coming from /chat: ${req.body}`);
+  res.cookie("character", req.body.character);
+  return res.redirect("/chat");
+});
+
 
 
 // Load up the server
@@ -28,7 +48,7 @@ app.listen(1337, () => {
 **
 ** Serve a page that sends post requests to the server
 **
-** Server renders the text and passes it back to the page
+** Server renders the text and a link to an icon and passes it back
 **
 ** https://socket.io/get-started/chat/ socket probably a good package for then showing it to other users
 **
